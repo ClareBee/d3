a15700794c4b24bbe8a937dd8be25c98fd872693
Clare <chblackburne@gmail.com>
2018-05-06 11:55:52 +0100

    Install d3 and display dummy data as circles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as d3 from 'd3';
+
+var expenses = [
+    {
+      name: 'Coffee',
+      amount: 4,
+      date: new Date()
+    },
+    {
+      name: 'Safeway',
+      amount: 20,
+      date: new Date()
+    },
+    {
+      name: 'Amazon',
+      amount: 15,
+      date: new Date()
+    },
+
+];
+let width = 900;
+let height = 900;
+let radius = 10;
+let simulation = d3.forceSimulation();
 
 class App extends Component {
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+    this.container = d3.select(this.refs.container);
+    // draw expenses circles
+    let circles = this.container.selectAll('circle')
+      .data(expenses, d => d.name);
+    // exit
+    circles.exit().remove();
+    // enter/update
+    circles = circles.enter().append('circle')
+      .merge(circles)
+      .attr('r', radius);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <svg width={width} height={height} ref="container">
+      </svg>
     );
   }
 }
