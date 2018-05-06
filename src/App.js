67eb04b459ab58f95e8b526d1b8ffc89fdfb0b9b
Clare <chblackburne@gmail.com>
2018-05-06 13:23:59 +0100

    Install chroma and use to color circles acc to amount

--- src/App.js
+++ src/App.js
@@ -1,32 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
 import * as d3 from 'd3';
-import expenses from './data/expenses';
+import chroma from 'chroma-js';
+import _ from 'lodash';
+import expensesData from './data/expenses';
 
+let colorScale = chroma.scale(['#53cf8d', '#f7d283' ,'#c85151']);
+let amountScale = d3.scaleLinear();
 let width = 900;
 let height = 900;
-let radius = 10;
+let radius = 20;
 let simulation = d3.forceSimulation()
   .force('center', d3.forceCenter(width / 2, height / 2))
-  // .force('charge', d3.forceManyBody())
+  .force('charge', d3.forceManyBody(-10))
   .force('collide', d3.forceCollide(radius))
   .stop();
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      expenses:[]
+    }
     this.forceTick = this.forceTick.bind(this);
     this.renderCircles = this.renderCircles.bind(this);
+    simulation.on('tick', this.forceTick);
   }
 
   componentWillMount() {
-    simulation.on('tick', this.forceTick);
+    let expenses = _.chain(expensesData)
+        .filter(d => d.amount > 0)
+        .map(d => {
+          return {
+            amount: -d.amount,
+            description: d.name,
+            date: d.date,
+          }
+        }).value();
+    this.setState({expenses});
+    // process data
+    let amountExtent = d3.extent(expenses, d => d.amount);
+    colorScale.domain(amountExtent);
   }
 
   componentDidMount() {
     this.container = d3.select(this.refs.container);
     this.renderCircles();
-    simulation.nodes(expenses).alpha(0.9).restart();
+    simulation.nodes(this.state.expenses).alpha(0.9).restart();
   }
 
   componentDidUpdate(){
@@ -36,19 +56,21 @@ class App extends Component {
   renderCircles(){
     // draw expenses circles
     this.circles = this.container.selectAll('circle')
-      .data(expenses, d => d.name);
+      .data(this.state.expenses, d => d.name);
     // exit
     this.circles.exit().remove();
     // enter/update
     this.circles = this.circles.enter().append('circle')
       .merge(this.circles)
       .attr('r', radius)
-      .attr('opacity', 0.5);
+      .attr('fill', d => colorScale(amountScale(d.amount)));
   }
+
   forceTick(){
     this.circles.attr('cx', d => d.x)
       .attr('cy', d => d.y);
   }
+
   render() {
     return (
       <svg width={width} height={height} ref="container">
